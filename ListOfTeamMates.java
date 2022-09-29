4ba09e2876913cf398626762f02b0a7bf7bd69e1
Argschadt <110493607+Argschadt@users.noreply.github.com>
2022-09-29 12:19:19 -0300

    Add files via upload

---
+++ ListOfTeamMates.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class ListOfTeamMates{
+	public static void main(String[] args) {
+		ArrayList<TeamMate> list = new ArrayList<TeamMate>();
+		
+		TeamMate a = new TeamMate("1", "Name 1", false);
+		list.add(a);
+		
+		for (int i = 0; i < 20; i++) {
+	      list.add(new TeamMate("" + (i+2), "Name " + (i+2), false));
+	    }
+		
+		for (int i = 0; i < 20; i++) {
+	      list.add(new TeamMate("" + (i+22), "Name " + (i+22), true));
+	    }
+		
+		System.out.println("---");
+	    for (int i = 0; i < list.size(); i++) {
+	      System.out.println(list.get(i).getName());
+	    }
+	    
+	    for (TeamMate item : list) {
+	    	if(item.online == true) {
+	    		System.out.println(item.getUserId());
+	    	}
+	    }
+	    
+	    list.remove(0);
+	    
+	    System.out.println("Elemento 0 removido");
+	    for (int i = 0; i < list.size(); i++) {
+	      System.out.println(list.get(i).getName());
+	    }
+	}
+}
